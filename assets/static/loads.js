c68ac33992ae9ae8533001af9e4ad3eba7b8b48f
Daniellyson <etu32290@henallux.be>
2020-09-18 16:23:19 +0200

    single in progress

---
+++ assets/static/loads.js
@@ -0,0 +1,13 @@
+window.onload = function() {    
+    // place where I fade out the loading page  <div class="se-pre-con"></div> under </body>
+    $(".se-pre-con").fadeOut("slow");
+}
+
+function fireGitHub() {
+    location.href = "https://github.com/Daniellyson";
+}
+
+function fireLinkedin() {
+    location.href = "https://www.linkedin.com/in/daniellyson-vasconcelos/";
+}
+
