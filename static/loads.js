97e9bec06f9033f2b199f27517f758ed480a59ab
Daniellyson <etu32290@henallux.be>
2020-09-22 12:22:50 +0200

    portfolio / css / tippy.js

--- static/loads.js
+++ static/loads.js
@@ -1,7 +1,6 @@
 window.onload = function() {    
     // place where I fade out the loading page  <div class="se-pre-con"></div> under </body>
     $(".se-pre-con").fadeOut("slow");
-    $("head").load("header.html");
 }
 
 function fireGitHub() {
--- static/tippies.js
+++ static/tippies.js
@@ -1,4 +1,7 @@
 tippy('#cLang', {
     content: 'C language is considered as the mother language of all the modern programming languages ' + 
-    'because most of the compilers, JVMs, Kernels, etc. are written in C'
+    'because most of the compilers, JVMs, Kernels, etc. are written in C',
+    placement: 'bottom',
+    theme: 'img',
+    interactive: true
 });
