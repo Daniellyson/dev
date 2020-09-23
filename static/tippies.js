adfc0b15210f753b6c3dc143786bdcf4c3ae565b
Daniellyson <etu32290@henallux.be>
2020-09-23 20:06:36 +0200

    phones

--- static/tippies.js
+++ static/tippies.js
@@ -2,6 +2,12 @@ tippy('#cLang', {
     content: 'C language is considered as the mother language of all the modern programming languages ' + 
     'because most of the compilers, JVMs, Kernels, etc. are written in C',
     placement: 'bottom',
-    theme: 'img',
-    interactive: true
+    theme: 'img'
+});
+
+tippy('#escape', {
+    content: 'One of my projects during my internship at ' + "<a class='tooltip-a' href='https://www.miil.be/'>MiiL</a>",
+    interactive: true,
+    placement: 'bottom',
+    theme: 'img'
 });
