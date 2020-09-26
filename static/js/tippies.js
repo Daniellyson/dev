1015a9c4364ae5af5c60b5d725fb18a2e9702c94
Daniellyson <etu32290@henallux.be>
2020-09-26 07:45:11 +0200

    portfolio

--- static/js/tippies.js
+++ static/js/tippies.js
@@ -12,12 +12,17 @@ function fireGitHubFuel() {
 
 tippy('#escape', {
     content: 'One of my developments during my internship at <span class="tippies" onclick="fireMiiL()">MiiL</span>.' +
-    ' An escape game to play on your PC, built for the platform <span class="tippies" onclick="fireUCL()">Smart\'Web</span>.',
+    ' An escape game to play on your PC, built for the platform <span class="tippies" onclick="fireUCL()">Smart\'Web</span> ' +
+    'built with <span class="tippies" onclick="fireUnity()">Unity</span>',
     interactive: true,
     placement: 'bottom',
     theme: 'sideBar',
     arrow: true
 });
+
+function fireUnity() {
+    location.href = "#unity";
+}
 
 function fireMiiL() {
     window.open("https://www.miil.be/");
@@ -30,6 +35,13 @@ function fireUCL() {
 tippy('#cLang', {
     content: 'C language is considered as the mother language of all the modern programming languages ' + 
     'because most of the compilers, JVMs, Kernels, etc. are written in C',
+    placement: 'bottom',
+    theme: 'language',
+    arrow: true
+});
+
+tippy('#unity', {
+    content: 'Unity 2D : This tech was not tought at IESN. I learnt, by my self, during my internship to be able to create the Escape Game',
     placement: 'bottom',
     theme: 'language',
     arrow: true
