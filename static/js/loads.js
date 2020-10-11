338bf1e75c1264482ebecb1642167ebac561d4c2
Daniellyson <etu32290@henallux.be>
2020-10-11 19:41:10 +0200

    Madeleine test

--- static/js/loads.js
+++ static/js/loads.js
@@ -1,6 +1,29 @@
 window.onload = function() {    
     // place where I fade out the loading page  <div class="se-pre-con"></div> under </body>
     $(".se-pre-con").fadeOut("slow");
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyDHJ9fadEOAU83Ftl4ORm0dpmgdk_9kdHo",
+        authDomain: "fir-js-ca028.firebaseapp.com",
+        databaseURL: "https://fir-js-ca028.firebaseio.com",
+        projectId: "fir-js-ca028",
+        storageBucket: "fir-js-ca028.appspot.com",
+        messagingSenderId: "62510889739",
+        appId: "1:62510889739:web:684a25a863053a68b8800e"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    database = firebase.database();
+    ref = database.ref("likes");
+
+    ref.on("value", function(snapshot) {
+
+        if(!isNaN(snapshot.val().love)) {
+            document.getElementById('like').innerHTML = snapshot.val().love;
+        }
+    });
 }
 
 function fireGitHub() {
@@ -10,3 +33,55 @@ function fireGitHub() {
 function fireLinkedin() {
     location.href = "https://www.linkedin.com/in/daniellyson-vasconcelos/";
 }
+
+function fireBaseUpdate() {
+
+    var myCookie = getCookie();
+
+    if(myCookie != "Madeleine") {
+        var plusOne = 0;
+
+        ref.on("value", function(snapshot){
+            if(!isNaN(snapshot.val().love)) {
+                plusOne = parseInt(snapshot.val().love);
+            }
+        });
+    
+        plusOne++;
+    
+        ref.set({
+            love: plusOne
+        });
+    
+        document.getElementById("like").innerHTML = plusOne;
+
+        setCookie();
+    }
+
+}
+
+function setCookie() {
+    var cname = "MyCookieIs";
+    var cvalue = "Madeleine";
+    var exdays = 30;
+    var date = new Date();
+    date.setTime(date.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ date.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";";
+}
+
+function getCookie(cname = "Madeleine") {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
